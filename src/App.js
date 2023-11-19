092affeff4947303801ba1a04bd8c1f24b2a7de4
grs55705 <132606108+grs55705@users.noreply.github.com>
2023-11-19 13:21:33 -0500

    Redadded Home view

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
+import Home from './components/Home';
 import AddItem from './components/AddItem'; // Add item view
 import './App.css';
 
@@ -8,7 +8,7 @@ function App() {
   return (
     <Router>
       <Routes>
-       
+        <Route path="/" element={<Home />} />
         <Route path="/" element={<AddItem />} />
       </Routes>
     </Router>
