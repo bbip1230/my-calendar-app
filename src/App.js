055760d465103af65e237332d376fcdfbfc26214
Siya Sharma <104864375+SiyaSharma20@users.noreply.github.com>
2023-11-20 00:37:47 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import './App.css';
 import AuthenticatedView from './pages/AuthenticatedView';
-
+import AddItem from './components/AddItem';
 function App() {
   //const navigate = useNavigate();
   return (
@@ -11,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="authenticatedview" element={<AuthenticatedView />} />
+        <Route path="/additem" element={<AddItem />} />
       </Routes>
     </Router>
   );
