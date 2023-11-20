bd2386fc7850a36be04547738611ceb15b441510
Siya Sharma <104864375+SiyaSharma20@users.noreply.github.com>
2023-11-20 00:02:27 -0500

    Add files via upload
    Updated App.js with new routes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
-import AddItem from './components/AddItem'; // Add item view
 import './App.css';
+import AuthenticatedView from './pages/AuthenticatedView';
 
 function App() {
+  //const navigate = useNavigate();
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/" element={<AddItem />} />
+        <Route path="authenticatedview" element={<AuthenticatedView />} />
       </Routes>
     </Router>
   );
 }
 
 export default App;
-
-
