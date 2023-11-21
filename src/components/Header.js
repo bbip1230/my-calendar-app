507ad72187846dc8588ecbcd4353a4d16cf1a84b
CSA-Snow <csa55400@uga.edu>
2023-11-20 19:29:00 -0500

    Made the links in non-auth header the same as auth and functional

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import './Header.css';
+import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
+  const navigate = useNavigate();
+  
   return (
     <header>
       <div className="logo">CalendarApp</div>
       <nav className="navigation">
         <ul>
-          <li>Home</li>
-          <li>Calendar</li>
-          <li>Info</li>
-          <li>Tasks</li>
-          <li>Settings</li>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/">My Items</Link>
+          </li>
+          <li>
+            <Link to="/">Add Item</Link>
+          </li>
         </ul>
         <button className="sign-in">Sign In</button>
       </nav>
