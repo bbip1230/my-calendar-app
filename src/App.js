28647c2afec87890e310d14631fbdfcbacd0cf8f
Siya Sharma <104864375+SiyaSharma20@users.noreply.github.com>
2023-12-02 20:46:46 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,3 @@
-/*
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './components/Home';
-import './App.css';
-import AuthenticatedView from './pages/AuthenticatedView';
-import AddItem from './components/AddItem';
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="authenticatedview" element={<AuthenticatedView />} />
-        <Route path="/additem" element={<AddItem />} />
-      </Routes>
-    </Router>
-  );
-}
-
-export default App;
-*/
-
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
