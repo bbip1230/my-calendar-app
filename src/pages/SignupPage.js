77d1aa501c4d70b21f7699dd50f6af720ce59781
Siya Sharma <104864375+SiyaSharma20@users.noreply.github.com>
2023-12-02 20:39:20 -0500

    Add files via upload

---
+++ src/pages/SignupPage.js
@@ -0,0 +1,80 @@
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import './SignupPage.css';
+import axios from 'axios';
+//import Header from '../components/Header';
+import AuthenticatedHeader from '../components/AuthenticatedHeader';
+
+const SignupPage = () => {
+  const [user, setUsers] = useState([]);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  const fetchUsers = () => {
+    axios.get('http://localhost:4000/register').then((res) => {
+      console.log(res.data);
+    });
+  };
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    axios
+      .post('http://localhost:4000/register', { email, password })
+      .then(() => {
+        alert('Registration Successful');
+        setEmail('');
+        setPassword('');
+        fetchUsers();
+        navigate('/');
+      })
+      .catch((error) => {
+        console.log('Unable to register user');
+      });
+  };
+
+  return (
+    <div className="signup-page">
+      <AuthenticatedHeader />
+      <div className="signup-container">
+        <h1>Sign Up</h1>
+        <form onSubmit={handleLogin}>
+          <div className="form-group">
+            <label htmlFor="email">Email address</label>
+            <input
+              type="email"
+              placeholder="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              placeholder="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+
+          <button
+            type="submit"
+            //onClick={() => navigate('/')}
+            className="signup-button"
+          >
+            Signup
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default SignupPage;
