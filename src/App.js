631a8b1f042926f1d51ea26c7ac20f875e4020e5
Siya Sharma <104864375+SiyaSharma20@users.noreply.github.com>
2023-12-02 20:42:22 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,3 +1,25 @@
+/*
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import './App.css';
+import AuthenticatedView from './pages/AuthenticatedView';
+import AddItem from './components/AddItem';
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="authenticatedview" element={<AuthenticatedView />} />
+        <Route path="/additem" element={<AddItem />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
+*/
+
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
@@ -5,20 +27,22 @@ import './App.css';
 import AuthenticatedView from './pages/AuthenticatedView';
 import AddItem from './components/AddItem';
 import ViewItems from './components/ViewItems';
-import NotFound from './components/NotFound'
+import SignupPage from './pages/SignupPage';
 
 function App() {
-  //const navigate = useNavigate();
-  //isAuth variable is passed as props, must be modified by log in or sign up page
-  var isAuth = false;
+  const isUserSignedIn = !!localStorage.getItem('token');
+
   return (
     <Router>
       <Routes>
-        <Route path="*" element={<NotFound loggedIn={isAuth} />} />
         <Route path="/" element={<Home />} />
-        <Route path="authenticatedview" element={<AuthenticatedView />} />
+
         <Route path="/additem" element={<AddItem />} />
         <Route path="/viewitems" element={<ViewItems />} />
+        <Route path="/signup" element={<SignupPage />} />
+        {isUserSignedIn && (
+          <Route path="authenticatedview" element={<AuthenticatedView />} />
+        )}
       </Routes>
     </Router>
   );
